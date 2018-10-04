487c69c850669b5ed1fe213e279be5dd1ff73b60
Staerdahl <peterandersen14@live.dk>
2018-10-04 09:25:43 +0200

    Sout

--- SYNchatServer/src/synchatserver/SYNchatServer.java
+++ SYNchatServer/src/synchatserver/SYNchatServer.java
@@ -15,7 +15,7 @@ public class SYNchatServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("hej");
+       System.out.println("hejsa");
     }
     
 }
