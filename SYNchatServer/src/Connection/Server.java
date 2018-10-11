50bebc05e8436d1b0cd0c863d400d40204273980
Staerdahl <peterandersen14@live.dk>
2018-10-11 11:34:08 +0200

    createConnection

--- SYNchatServer/src/Connection/Server.java
+++ SYNchatServer/src/Connection/Server.java
@@ -6,13 +6,17 @@
 package Connection;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 
 /**
  *
@@ -21,6 +25,7 @@ import javax.net.ssl.SSLServerSocketFactory;
 public class Server implements Runnable{
 
     private int port;
+     private boolean isStopped = false;
     ExecutorService threadpool = Executors.newFixedThreadPool(10);
     InetAddress ip;
     SSLServerSocket sslServerSocket;
@@ -29,7 +34,6 @@ public class Server implements Runnable{
     public Server(InetAddress ip, int port){
         this.port = port;
         this.ip = ip;
-        System.out.println(createSocket());
     }
     
     
@@ -50,7 +54,77 @@ public class Server implements Runnable{
     
     @Override
     public void run() {
+        System.out.println(createSocket());
         System.out.println("Det kører!");
+        while(!isStopped()) {
+            SSLSocket clientSocket;
+             try {
+                clientSocket = (SSLSocket) this.sslServerSocket.accept(); // Wait for connection and accept
+            } catch (IOException e) {
+                throw new RuntimeException("Error accepting client connection", e);
+            }
+             handleConnection(clientSocket);
+            
+        }
     }
+    
+    
+        private void handleConnection(SSLSocket client) {
+            threadpool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    //TODO
+                    /*
+             try {       
+            InputStream input  = client.getInputStream();
+            OutputStream output = client.getOutputStream();
+
+            long time = System.currentTimeMillis();
+            String httpResponse = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html\r\n\r\nHello visitor number " + "\r\n";
+
+            output.write(httpResponse.getBytes("UTF-8"));
+
+            client.shutdownOutput();
+            output.close();
+            input.close();
+
+            System.out.println("Request processed: " + time);
+
+            // Simulate a connection lasting 10 seconds
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            //report exception somewhere.
+            e.printStackTrace();
+        }
+*/
+                }
+            }
+            );
+    }
+    
+    
+    
+    
+      private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop(){
+        this.isStopped = true;
+        try {
+            System.out.println("Closing web server");
+            this.sslServerSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+    
+    
+    
     
 }
