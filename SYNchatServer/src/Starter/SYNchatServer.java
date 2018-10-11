fb3657df6a88ace78be4131ee2960429fa9e7900
Rasje17 <32257308+Rasje17@users.noreply.github.com>
2018-10-11 11:12:37 +0200

    now fixed

--- SYNchatServer/src/Starter/SYNchatServer.java
+++ SYNchatServer/src/Starter/SYNchatServer.java
@@ -1,22 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Starter;
 
-/**
- *
- * @author Peter
- * Test server
- */
-public class SYNchatServer {
+import Acquaintance.IBusiness;
+import Acquaintance.IConnection;
+import Acquaintance.IPersistence;
+import Business.BusinessFacade;
+import Connection.ConnectionFacade;
+import Persistence.PersistenceFacade;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+public class SYNchatServer {
     public static void main(String[] args) {
-       System.out.println("hejsas");
+        IBusiness business = BusinessFacade.getInstance();
+        IConnection con = ConnectionFacade.getInstance();
+        IPersistence per = PersistenceFacade.getInstance();
+        
+        business.injectPersistence(per);
+        con.injectBusiness(business);
+        
+        
     }
-    
+
 }
