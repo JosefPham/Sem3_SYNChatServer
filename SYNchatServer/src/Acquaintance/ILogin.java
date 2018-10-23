979dd57e3c0a0625c90e15491df956b8f0e23741
Rasje17 <32257308+Rasje17@users.noreply.github.com>
2018-10-23 09:36:42 +0200

    fixed ilogin

--- SYNchatServer/src/Acquaintance/ILogin.java
+++ SYNchatServer/src/Acquaintance/ILogin.java
@@ -1,6 +1,15 @@
 package Acquaintance;
 
-
 public interface ILogin {
-    int Login(String hemail, String hpw);
+
+    public String gethMail();
+
+    public String gethPW();
+    
+        
+    public int getLoginvalue();
+    
+    public IUser getUser();
+
+    public int login(int loginValue);
 }
