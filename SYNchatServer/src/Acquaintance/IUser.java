a57b6e6649c2ffd1d742a06a8e8f857c6137928d
Rasje17 <32257308+Rasje17@users.noreply.github.com>
2018-10-23 09:50:19 +0200

    updated IUser

--- SYNchatServer/src/Acquaintance/IUser.java
+++ SYNchatServer/src/Acquaintance/IUser.java
@@ -1,6 +1,12 @@
 package Acquaintance;
 
+import java.util.List;
+
 
 public interface IUser {
-    
+    public int getUserID();
+    public String getTmpName();
+    public boolean isBanned();
+    public int getReports();
+    public List<Integer> getChats();
 }
